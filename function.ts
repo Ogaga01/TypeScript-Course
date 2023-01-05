a3363eca480eda6b9243b0c036b060c648e15a3d
Ogaga <iyaraogaga69@gmail.com>
2023-01-05 16:26:23 +0100

    More on functions

--- function.ts
+++ function.ts
@@ -4,4 +4,20 @@ const loginUser = (name: string, email: string, isPaid: boolean = false) => {
 
 loginUser('Ogee', 'ogig')
 
+const addNum = (amt: number): number => {
+    return amt + 2
+}
+
+addNum(2)
+
+// function myVal(val: number): boolean {
+//     if (val > 5) {
+//         return true
+//     } else {
+//         return 'Val is < five'
+//     }
+// }
+
+// myVal(6)
+
 export {}
