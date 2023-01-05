4978ebd6b88944743b10b8cdb1e4a52e0e8b6633
Ogaga <iyaraogaga69@gmail.com>
2023-01-05 13:35:42 +0100

    Add basics

---
+++ intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'Anwar', age: 20 };
+console.log('Ogaga');
+console.log(user.name);
---
+++ intro.ts
@@ -0,0 +1,4 @@
+let user = {name: 'Anwar', age: 20}
+
+console.log('Ogaga')
+console.log(user.name)
